99a77d2fee1c545b511765e4849b8c4af5cc3606
Peter Deme <snypox@gmail.com>
2025-03-26 14:25:49 +0100

    Fix bugs in the IAM module (#15)
    Signed-off-by: peterdeme <snypox@gmail.com>

--- modules/ecs/iam_execution.tf
+++ modules/ecs/iam_execution.tf
@@ -8,19 +8,11 @@ resource "aws_iam_role" "execution" {
 resource "aws_iam_role_policy" "execution" {
   for_each = var.execution_role_arn == null ? module.iam_roles_and_policies.execution.policies : {}
 
-  name   = "${aws_iam_role.execution[0].name}-${each.key}"
   role   = aws_iam_role.execution[0].name
   policy = each.value
 }
 
 resource "aws_iam_role_policy_attachment" "execution" {
-  for_each = var.execution_role_arn == null ? module.iam_roles_and_policies.execution.policies : {}
-
-  role       = aws_iam_role.execution[0].name
-  policy_arn = aws_iam_role_policy.execution[each.key].arn
-}
-
-resource "aws_iam_role_policy_attachment" "execution_extra" {
   for_each = var.execution_role_arn == null ? module.iam_roles_and_policies.execution.attachments : {}
 
   role       = aws_iam_role.execution[0].name
